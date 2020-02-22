cc31c7b0a9da4404d73dd97ea5499ed4d30d48cb
chinhTEO <mrtruongchinhnguyen@gmail.com>
2020-02-22 12:12:03 +0700

    create index end point

--- django_bds_backend/django_bds_backend/settings.py
+++ django_bds_backend/django_bds_backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'RESTauth'
+    'rest_framework',
+    'RESTauth',
 ]
 
 MIDDLEWARE = [
