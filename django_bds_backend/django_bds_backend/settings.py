da655da4e25261345abba322adb2ac511bf290ba
chinhTEO <mrtruongchinhnguyen@gmail.com>
2020-02-22 07:29:12 +0700

    add RESTauth and runserver

--- django_bds_backend/django_bds_backend/settings.py
+++ django_bds_backend/django_bds_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'afv+gqy$xg2=t!nnaukg($%harqr9l3-)#yykx^!*50ifl5(#)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'RESTauth'
 ]
 
 MIDDLEWARE = [
